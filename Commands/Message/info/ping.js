fddc3a80d8de7bdfe3d09aff607bc20a11db7cde
Kalai2773 <87932746+Kalai2773@users.noreply.github.com>
2022-08-20 14:56:38 +0530

    Add files via upload

---
+++ Commands/Message/info/ping.js
@@ -0,0 +1,22 @@
+const { Client, Message, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+  name: "ping",
+  description: "get ping of bot",
+  category: "info",
+  userPermissions: [PermissionFlagsBits.Administrator],
+  botPermissions: [PermissionFlagsBits.Administrator],
+
+  /**
+   *
+   * @param {Client} client
+   * @param {Message} message
+   */
+  run: async (client, message) => {
+    // code
+    message.reply({
+      content: (`**Pong! ${client.ws.ping}ms**`),
+    })
+     
+  },
+};
---
+++ settings/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    TOKEN : process.env.TOKEN || "OTUzMjM0ODQ3OTg2NDkxMzky.GLrZCf.yT-1C0d30U3qcMKnHR04OiavpaHVOGl57jmXIE",
+    PREFIX : process.env.PREFIX || ","
+}
