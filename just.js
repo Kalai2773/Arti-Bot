717f71b2f2160b2b2254a4bbe9e6cdf4499acfa9
Kalai2773 <87932746+Kalai2773@users.noreply.github.com>
2022-08-20 14:54:30 +0530

    Delete just.js

--- just.js
+++
@@ -1,9 +0,0 @@
-const {Client , GatewayIntentBits}= require("discord.js");
-const client = new Client({ intents: [GatewayIntentBits.Guilds]});
-const config = require("/config.js");
-
-client.on("ready",() => {
-    console.log(" i am still alive");
-});
-
-client.login(config.token);
